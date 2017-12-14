daa5a5e2185b22add2686d7b6365ca57f0004745
ilya89099 <32363840+ilya89099@users.noreply.github.com>
2017-12-14 16:12:32 +0300

    карта

---
+++ scripts.js
@@ -0,0 +1,53 @@
+var openModal = document.querySelector(".write-us");
+var closeModal1 = document.querySelector(".cm1")
+var closeModal2 = document.querySelector(".cm2")
+var modalWindow = document.querySelector(".write-us-modal")
+var modalMap = document.querySelector(".modal-map")
+
+openModal.addEventListener("click", function() {
+    modalWindow.classList.add("opened");
+})
+
+closeModal1.addEventListener("click", function() {
+    modalWindow.classList.remove("opened");
+})
+
+document.getElementById('map1').addEventListener('mousedown', function() {
+    modalMap.classList.add("opened");
+})
+
+closeModal2.addEventListener("click", function() {
+    modalMap.classList.remove("opened")
+})
+
+
+
+function initMap() {
+    var place = {
+        lat: 59.938759,
+        lng: 30.3231015
+    };
+
+
+    var map1 = new google.maps.Map(document.getElementById('map1'), {
+        zoom: 19,
+        center: place
+    });
+    var map2 = new google.maps.Map(document.getElementById('map2'), {
+        zoom: 19,
+        center: place
+    });
+    var marke1 = new google.maps.Marker({
+        position: place,
+        map: map1,
+        icon: ("images/b3.png"),
+        title: "Hello World!"
+    });
+
+    var marker2 = new google.maps.Marker({
+        position: place,
+        map: map2,
+        icon: ("images/b3.png"),
+        title: "Hello World!"
+    });
+}
